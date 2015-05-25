a5007205500192093e9766f3917192542ce1ec88
Sho Iizuka <sho.i518@gmail.com>
2015-05-25 10:24:59 +0900

    modified adult/ml.py

--- adult/ml.py
+++ adult/ml.py
@@ -1,8 +1,31 @@
 # coding: utf-8
+from __future__ import division, print_function, unicode_literals
 import pandas as pd
+
+def contains_na_columns(d):
+    """
+    欠損値を含む列のpandas.Indexを返す
+
+    Parameters
+    ----------
+    d : pandas.DataFrame
+    """
+    return d.columns.difference(d.dropna(axis=1).columns)
+
+# ----------------
+# データの読み込み
+# ----------------
 d = pd.read_csv(
     'adult.data', sep=', ',
     names=('age', 'workclass', 'fnlwgt', 'education', 'education_num',
            'marital_status', 'occupation', 'relationship', 'race', 'sex',
            'capital_gain', 'capital_loss', 'hours_per_week', 'native_country', 'label'),
-    engine='python')
+    engine='python',
+    na_values='?')
+
+# ------------
+# データの加工
+# ------------
+# In [ ]: contains_na_columns(d)
+# Out[ ]: Index([u'native_country', u'occupation', u'workclass'], dtype='object')
+d['label'] = (d['label'] == '>50K')
