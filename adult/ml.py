4b0413e9464fd2a8aa9912471a4c1353f9a80787
Sho Iizuka <sho.i518@gmail.com>
2015-05-26 10:47:41 +0900

    modified ml.py

--- adult/ml.py
+++ adult/ml.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 from __future__ import division, print_function, unicode_literals
+import numpy
 import pandas as pd
+from sklearn.preprocessing import LabelBinarizer, StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn_pandas import DataFrameMapper, RandomizedSearchCV
 
 def contains_na_columns(d):
     """
@@ -12,9 +17,12 @@ def contains_na_columns(d):
     """
     return d.columns.difference(d.dropna(axis=1).columns)
 
-# ----------------
+def best_cv_num(n):
+    return int(1+numpy.log2(n))
+
+#
 # データの読み込み
-# ----------------
+#
 d = pd.read_csv(
     'adult.data', sep=', ',
     names=('age', 'workclass', 'fnlwgt', 'education', 'education_num',
@@ -23,9 +31,35 @@ d = pd.read_csv(
     engine='python',
     na_values='?')
 
-# ------------
+#
 # データの加工
-# ------------
+#
 # In [ ]: contains_na_columns(d)
 # Out[ ]: Index([u'native_country', u'occupation', u'workclass'], dtype='object')
-d['label'] = (d['label'] == '>50K')
+d.loc[:,'label'] = (d.loc[:,'label'] == '>50K')
+_cols = contains_na_columns(d)
+d.loc[:,_cols] = d.loc[:,_cols].fillna('?')
+
+mapper = DataFrameMapper([
+    ('age', StandardScaler()),
+    ('workclass', LabelBinarizer()),
+    ('fnlwgt', StandardScaler()),
+    ('education', LabelBinarizer()),
+    ('education_num', StandardScaler()),
+    ('marital_status', LabelBinarizer()),
+    ('occupation', LabelBinarizer()),
+    ('relationship', LabelBinarizer()),
+    ('race', LabelBinarizer()),
+    ('sex', LabelBinarizer()),
+    ('capital_gain', StandardScaler()),
+    ('capital_loss', StandardScaler()),
+    ('hours_per_week', StandardScaler()),
+    ('native_country', LabelBinarizer()),
+    ])
+p = Pipeline([
+    ('mapper', mapper),
+    ('clf', LinearSVC()),
+    ])
+_n = d.shape[0]
+cv = RandomizedSearchCV(p, {'clf__C': 2**numpy.linspace(-5,3,1000)}, n_iter=10, cv=best_cv_num(_n), n_jobs=-1, verbose=3)
+cv.fit(d, d.loc[:,'label'].values)
